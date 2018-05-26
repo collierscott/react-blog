92f7c57380297ca99153b9057fed97251ea91292
Scott Collier <scott@onlinespaces.com>
2018-05-26 15:29:26 +0200

    Add notes about BrowserRouter basename property

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // Can set a basename property to tell app the basename of site.
+      // This is useful if site is in a subdirectory
       <BrowserRouter>
         <div className="App">
           <Blog />
