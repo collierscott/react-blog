bb4557bbba6299ddc5990cecc6b9770c54ecfda4
Scott Collier <scott@onlinespaces.com>
2018-05-26 12:45:57 +0200

    Restructure so that post containers are components

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,74 +1,28 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
 import axios from '../../axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from './Posts/Posts';
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    };
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0,3);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Scott',
-                        summary: post.body.substr(0, 200)
-                    }
-                });
-
-                this.setState({posts: updatedPosts});
-            })
-            .catch(error => {
-                // Errors here are like no internet connection and stuff
-                console.log(error);
-                this.setState({error: true});
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    };
-
     render () {
-        let posts = <p> </p>;
-
-        if(this.state.error) {
-            posts = <p style={{textAlign: 'center'}}>Error!</p>;
-        } else {
-            posts = this.state.posts.map(
-                post => {
-                    return <Post
-                        key={post.id}
-                        title={post.title}
-                        author={post.author}
-                        summary={post.summary}
-                        body={post.body}
-                        clicked={() => this.postSelectedHandler(post.id)}
-                    />
-                }
-            );
-        }
-
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Posts/>
+                {/* <section>
                     <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
-                </section>
+                </section> */}
             </div>
         );
     }
